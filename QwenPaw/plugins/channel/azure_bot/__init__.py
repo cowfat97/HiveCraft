26c1d1a7f29a675dc96c9c6b94be0b2f75801f1a
haoxinlei <howxl97@163.com>
2026-07-25 14:03:33 +0800

    temp: remove QwenPaw for subtree add

--- QwenPaw/plugins/channel/azure_bot/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Azure Bot Service channel for QwenPaw."""
-
-from .channel import AzureBotChannel
-
-__all__ = ["AzureBotChannel"]
--- QwenPaw/src/qwenpaw/agents/context/scroll/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Scroll context strategy: durable history + eviction index + recall REPL."""
